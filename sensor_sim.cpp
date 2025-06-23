a260251b43b63c980220be5b49b3e34b4c7f2bd3
Pantelis Pantelidis <rockytelis@hotmail.com>
2025-06-23 14:12:51 +0100

    Initial commit: add distance sensor simulator

---
+++ sensor_sim.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <thread>  // for sleep
+#include <chrono>
+
+int main() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    while (true) {
+        int distance = 100 + std::rand() % 50;
+        std::cout << "Distance: " << distance << " cm" << std::endl;
+
+        // Sleep for ~300ms between readings
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    }
+
+    return 0;
+}
