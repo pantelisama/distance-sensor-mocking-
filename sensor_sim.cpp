c6945e3cf9fabe44fd550fe65fa6f6dd449d73fb
Pantelis Pantelidis <rockytelis@hotmail.com>
2025-06-23 15:42:12 +0100

    Fix: force cout flush for subprocess compatibility

--- sensor_sim.cpp
+++ sensor_sim.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <thread>
 #include <chrono>
+#include <thread>
 
 int main() {
-    std::ios::sync_with_stdio(true);  // use C-style flushing
+    std::setvbuf(stdout, NULL, _IONBF, 0); // force no buffering
+    std::srand(static_cast<unsigned>(std::time(0)));
 
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-
-    while (true) {
-        int distance = 100 + std::rand() % 50;
-
-        // Flush with \n and force stdio to flush
-        std::printf("Distance: %d cm\n", distance);
-        std::fflush(stdout);  // force flush now
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "Distance: " << (std::rand() % 100) << " cm" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
-
     return 0;
 }
